eb5202b76c8082790b0a51ed6a73efc87a06641b
Chalermpan Sukumalwong <46768647+zeroheartza@users.noreply.github.com>
2023-11-20 18:18:27 +0700

    Add files via upload

--- InternalMachineIssueReport/InternalMachineIssueReport/urls.py
+++ InternalMachineIssueReport/InternalMachineIssueReport/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from cores.views import Machine
 
 urlpatterns = [
 path('admin/', admin.site.urls),
 path('cores/', include('cores.urls')),
+path("/", Machine.MachineAPIView.as_view(), name="student_score"),
+path("", Machine.MachineAPIView.as_view(), name="student_score"),
 ]
--- InternalMachineIssueReport/cores/urls.py
+++ InternalMachineIssueReport/cores/urls.py
@@ -6,6 +6,7 @@ from django.urls import path
 from cores.views import Machine
 
 urlpatterns = [
+    path("/", Machine.MachineAPIView.as_view(), name="student_score"),
 
     path("register_machine/", Machine.MachineAPIView.as_view(), name="student_score"),
    
